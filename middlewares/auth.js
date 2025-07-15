10621cc2fa818d75a52ad874fc50d2a1c145b68e
jodie-downey <jodie.downey1015@gmail.com>
2025-07-15 06:31:21 -0500

    Postman testing updates

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,27 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = require("../utils/config");
+const { JWT_SECRET } = require("../utils/config");
 const { UNAUTHORIZED_STATUS_CODE } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log("Auth middleware hit");
 
   if (!authorization || !authorization.startsWith("Bearer")) {
     return res
       .status(UNAUTHORIZED_STATUS_CODE)
       .send({ message: "Authorization Required" });
   }
-  const token = authorization.replace("Bearer", " ");
+
+  const token = authorization.replace("Bearer ", "");
   let payload;
+
+  console.log(token);
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
+    console.log("JWT verified successfully:", payload);
   } catch (err) {
+    console.log("JWT verification error:", err);
     return res
       .status(UNAUTHORIZED_STATUS_CODE)
       .send({ message: "Authorization Required" });
@@ -23,5 +29,5 @@ module.exports = (req, res, next) => {
 
   req.user = payload;
 
-  next();
+  return next();
 };
