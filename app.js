3d015c9131afe6f651f7fbd2dbad0ade18bec159
jodie-downey <jodie.downey1015@gmail.com>
2025-08-21 12:13:17 -0500

    signin auth corrected

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 app.post("/signin", login);
 app.post("/signup", createUser);
-app.use(auth);
 app.use("/", mainRouter);
+app.use(auth);
 
+console.log("listening port 3001");
 const { PORT = 3001 } = process.env;
 
 app.listen(PORT, () => {});
