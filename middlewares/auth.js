76e8fd9901c65d4c9874f046dfa4ff22c8d4b838
jodie-downey <jodie.downey1015@gmail.com>
2025-09-21 11:10:14 -0500

    auth 401 middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,11 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { UnauthorizedError } = require("../errors/UnauthorizedError");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer")) {
-    return res
-      .status(UnauthorizedError)
-      .send({ message: "Authorization Required" });
+    return res.status(401).send({ message: "Authorization Required" });
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -17,9 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res
-      .status(UnauthorizedError)
-      .send({ message: "Authorization Required" });
+    return res.status(401).send({ message: "Authorization Required" });
   }
 
   req.user = payload;
