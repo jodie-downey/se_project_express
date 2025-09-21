659c6307519423e9733d56a17498f0104f70473c
jodie-downey <jodie.downey1015@gmail.com>
2025-09-21 10:36:57 -0500

    auth code update in auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { UNAUTHORIZED_STATUS_CODE } = require("../utils/errors");
+const { UnauthorizedError } = require("../errors/UnauthorizedError");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer")) {
     return res
-      .status(UNAUTHORIZED_STATUS_CODE)
+      .status(UnauthorizedError)
       .send({ message: "Authorization Required" });
   }
 
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     return res
-      .status(UNAUTHORIZED_STATUS_CODE)
+      .status(UnauthorizedError)
       .send({ message: "Authorization Required" });
   }
 
